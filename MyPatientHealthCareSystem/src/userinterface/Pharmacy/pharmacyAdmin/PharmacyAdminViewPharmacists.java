84fb5c6ad598f95c41c1dc500df23861786f828c
Tejas Choudhari <choudhari.t@northeastern.edu>
2022-12-11 16:34:27 -0500

    Fix Bug

--- MyPatientHealthCareSystem/src/userinterface/Pharmacy/pharmacyAdmin/PharmacyAdminViewPharmacists.java
+++ MyPatientHealthCareSystem/src/userinterface/Pharmacy/pharmacyAdmin/PharmacyAdminViewPharmacists.java
@@ -76,7 +76,7 @@ public class PharmacyAdminViewPharmacists extends javax.swing.JPanel {
 
         jLabel1.setFont(new java.awt.Font("Georgia", 0, 24)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("View Technician Information");
+        jLabel1.setText("View Pharmacist Information");
         add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 20, 1200, 37));
 
         pharTable.setModel(new javax.swing.table.DefaultTableModel(
